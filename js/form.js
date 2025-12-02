fddda9e0990c1ebf25ac26f3954aa398239e8692
Illenium25 <146192625+newunravel@users.noreply.github.com>
2025-12-02 10:14:56 -0800

    form

---
+++ js/form.js
@@ -0,0 +1,29 @@
+document.getElementById("cleanerForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const button = form.querySelector(".form__btn");
+  const successMsg = document.getElementById("formSuccess");
+
+  // кнопка — “Sending...”
+  button.classList.add("loading");
+  button.textContent = "Sending...";
+
+  // собираем данные формы
+  const formData = new FormData(form);
+
+  // отправляем AJAX-запрос
+  const response = await fetch("https://formsubmit.co/teodoroscleaner@gmail.com", {
+    method: "POST",
+    body: formData
+  });
+
+  // успех
+  if (response.ok) {
+    form.reset();
+    successMsg.style.display = "block";
+
+    button.classList.remove("loading");
+    button.textContent = "Send Request";
+  }
+});
